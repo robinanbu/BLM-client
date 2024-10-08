bf544baef60fdee4e55e727af686a5bb91ae97e4
robinanbu <robinanbu23@gmail.com>
2024-10-08 10:11:25 +0530

    fixed the issue of deployment not found.

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,15 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './assets/css/index.css'
+import { createRoot } from 'react-dom/client';
+import App from './App';
+import './assets/css/index.css';
+import { StrictMode } from 'react';
 
-createRoot(document.getElementById('root')).render(
-    <App />
-)
+const rootElement = document.getElementById('root');
+if (rootElement) {
+    createRoot(rootElement).render(
+        <StrictMode>
+            <App />
+        </StrictMode>
+    );
+} else {
+    console.error('Root element not found');
+}
